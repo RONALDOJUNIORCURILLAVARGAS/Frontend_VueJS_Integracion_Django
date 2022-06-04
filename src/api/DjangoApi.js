d516cb489da73e69d322bf6b38dab02ed21ac116
RONALDOJUNIORCURILLAVARGAS <ronaldocurillavargas@gmail.com>
2022-06-04 15:50:06 -0500

    first

--- src/api/DjangoApi.js
+++ src/api/DjangoApi.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 const DjangoApi = axios.create({
-    baseURL:'http://localhost:8000/'
+   // baseURL:'http://localhost:8000/'
+    baseURL:'https://backend-vuejs-django.herokuapp.com/'
 })
 export default DjangoApi
